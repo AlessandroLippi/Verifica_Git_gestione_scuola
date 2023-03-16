3afabdfb613413339e3936a304ad61fa15b095b8
Alessandro Lippi <ALESSANDRO.LIPPI@local.ittcesena.it>
2023-03-16 08:44:29 +0100

    implementata la classe alunno

--- Verifica Git Gestione_Scuola/alunno.cs
+++ Verifica Git Gestione_Scuola/alunno.cs
@@ -8,5 +8,34 @@ namespace Verifica_Git_Gestione_Scuola
 {
     internal class alunno
     {
+        string nome { get; set; }
+        string cognome { get; set; }
+        string residenza { get; set; }
+        int eta { get; set; }
+
+        internal alunno(string nome, string cognome, string residenza, int eta)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.residenza = residenza;
+            this.eta = eta;
+        }
+
+        // metodi: 
+
+        public string ToStringAlunno()
+        {
+            return  "dati dell'alunno :" + this.nome+" "+this.cognome + " "+this.residenza + " " +this.eta.ToString();
+        }
+
+        public void Classe()
+        {
+            alunno a = new alunno("Alessandro", "Lippi", "Meldola", 18);
+            if (a.eta <= 16)
+                Console.WriteLine("L'alunno " + a.nome + "frequenta il biennio");
+            else
+                Console.WriteLine("L'alunno " + a.nome + "frequenta il triennio");
+
+        }
     }
 }
