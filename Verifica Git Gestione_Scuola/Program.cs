db5f3bb8a37b26b121bd2006adc3c0b9a418e735
Alessandro Lippi <ALESSANDRO.LIPPI@local.ittcesena.it>
2023-03-16 09:37:18 +0100

    codice migliorato

--- Verifica Git Gestione_Scuola/Program.cs
+++ Verifica Git Gestione_Scuola/Program.cs
@@ -10,6 +10,18 @@ namespace Verifica_Git_Gestione_Scuola
     {
         static void Main(string[] args)
         {
+            alunno a = new alunno("Matteo", "Rossi", "Meldola", 17);
+            Scuola s = new Scuola("Ed mondo de amicis", "Via cavour 14", "dal lunedi al sabato dalle 7:55-13:00; 14:30-16:30");
+
+
+            a.ToStringAlunno();
+            s.AggiungiAlunno(a);
+            s.RicercaPerEtà();
+            s.RicercaPerCognome();
+            s.AlunniPresenti();
+
+            Console.ReadKey();
+
         }
     }
 }
---
+++ Verifica Git Gestione_Scuola/Scuola.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verifica_Git_Gestione_Scuola
+{
+    public class Scuola
+    {
+        string nome { get; set; }
+        string indirizzo { get; set; }
+        string orario { get; set; }
+        List<alunno> alunni = new List<alunno>(); 
+
+            public Scuola(string nome, string cognome, string orario)
+            {
+                this.nome = nome;
+                this.indirizzo = cognome;
+                this.orario = orario;
+
+            }
+
+        public void AggiungiAlunno(alunno a)
+        {
+            alunni.Add(a);
+        }
+        //metodo che gestisce la ricerca degli alunni(da completare perchè da problemi nel foreach):
+
+        public void RicercaPerCognome()
+        {
+            Console.WriteLine("inserire il cognome dell'alunno da ricercare:");
+            string Ricarcato = Console.ReadLine();
+            foreach (alunno x in alunni)
+            {
+                if (x.cognome == Ricarcato)
+                    Console.WriteLine(x.ToStringAlunno());
+                break;
+
+            }
+        }
+
+
+        //metodo di ricerca per l'età:
+
+        public void RicercaPerEtà()
+        {
+            Console.WriteLine("inserire l'età dell'alunno da ricercare:");
+            int EtàRicarcato = int.Parse(Console.ReadLine());
+            foreach (alunno x in alunni)
+            {
+                if (x.eta == EtàRicarcato)
+                    Console.WriteLine(x.ToStringAlunno());
+                break;
+
+            }
+        }
+        //metodo di controllo di tutti gli alunni presenti : 
+
+        
+        public void AlunniPresenti()
+        {
+            foreach(alunno x in alunni)
+            {
+                Console.WriteLine(x.ToStringAlunno());
+            }
+        }
+
+
+
+
+    }
+}
