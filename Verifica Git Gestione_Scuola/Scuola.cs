49e49fe1288aec4d35d67d4eaffaddbbc9d11def
Alessandro Lippi <ALESSANDRO.LIPPI@local.ittcesena.it>
2023-03-16 09:00:14 +0100

    implementata la classe Scuola

---
+++ Verifica Git Gestione_Scuola/Scuola.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verifica_Git_Gestione_Scuola
+{
+    public class Scuola
+    {
+        string nome { get; set; }
+        string cognome { get; set; }
+        int orario { get; set; }
+        List<alunno> alunni = new List<alunno>(); 
+
+            public Scuola(string nome, string cognome, int orario)
+            {
+                this.nome = nome;
+                this.cognome = cognome;
+                this.orario = orario;
+
+            }
+
+        public void AggiungiAlunno()
+        {
+            alunno a = new alunno("Alessandro", "Lippi", "Meldola", 18);
+            alunni.Add(a);
+        }
+        //metodo che gestisce la ricerca degli alunni:
+
+        //public string RicercaPerCognome()
+        //{
+        //    foreach(int alunno in alunni)
+        //    {
+        //        if(alunni[alunno].cognome)
+        //    }
+        //}
+
+
+        //metodo di ricerca per l'età:
+
+
+
+
+
+        //metodo di controllo di tutti gli alunni presenti : 
+
+
+
+    }
+}
---
+++ Verifica Git Gestione_Scuola/alunno.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verifica_Git_Gestione_Scuola
+{
+    public class alunno
+    {
+        string nome { get; set; }
+        string cognome { get; set; }
+        string residenza { get; set; }
+        int eta { get; set; }
+
+        public alunno(string nome, string cognome, string residenza, int eta)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.residenza = residenza;
+            this.eta = eta;
+        }
+
+        // metodi: 
+
+        public string ToStringAlunno()
+        {
+            return  "dati dell'alunno :" + this.nome+" "+this.cognome + " "+this.residenza + " " +this.eta.ToString();
+        }
+
+        public void Classe()
+        {
+            alunno a = new alunno("Alessandro", "Lippi", "Meldola", 18);
+            if (a.eta <= 16)
+                Console.WriteLine("L'alunno " + a.nome + "frequenta il biennio");
+            else
+                Console.WriteLine("L'alunno " + a.nome + "frequenta il triennio");
+
+        }
+    }
+}
